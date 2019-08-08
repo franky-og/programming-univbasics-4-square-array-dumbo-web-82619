a7eec4e78b0a15b6a07c5fd35754608fa773a52b
Franky Liu <cuifranky95@gmail.com>
2019-08-08 22:32:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
 counter = 0
   new_array = []
   while counter <= array.length
-  new_array << (array[counter] ** 2)
+  new_array.push (array[counter] ** 2)
   counter = counter + 1
 end
 return new_array
